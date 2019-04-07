fb1453028b694ec2da7a1a14d8cd7ec5a2272445
Kartikei-12 <kartikeimittal@gmail.com>
2019-04-07 23:02:00 +0530

    Getting started with project

---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+@author: Kartikei Mittal
+@email: kartikeimittal@gmail.com
+main.py file for attempting to Implement graph algorithm using Machine learing
+'''
+
+__author__ = 'Kartikei Mittal'
+__email__ = 'kartikeimittal@gmail.com'
+__version__ = '0.1d.'
+
+print('Hello World!!')
